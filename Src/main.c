7e94757df807fa838cb2e7329ee0e6d9f87b66d0
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2021-02-16 00:37:28 -0300

    Implements ITM_sendchar for debugging

--- Src/main.c
+++ Src/main.c
@@ -18,13 +18,14 @@
  */
 
 #include <stdint.h>
-
+#include <stdio.h>
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
-    /* Loop forever */
+	printf("Peripheral Interrupt Exercise\n");
+
 	for(;;);
 }
