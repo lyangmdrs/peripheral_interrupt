338d289ad0556587dd9b41a370fee1470c6164f8
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2021-02-16 01:02:27 -0300

    Pend and enable the interruption to USART3

--- Src/main.c
+++ Src/main.c
@@ -19,13 +19,30 @@
 
 #include <stdint.h>
 #include <stdio.h>
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
+
+#define USART3_IRQNO 39
 
 int main(void)
 {
 	printf("Peripheral Interrupt Exercise\n");
 
+	// Manually pend the pending bit the pending bit for the USART3 IRQ number in NVIC
+	uint32_t *pISPR1 = (uint32_t*)0xE000E204;
+	*pISPR1 |= (1 << (USART3_IRQNO % 32));
+
+	// Enable the USART3 number in NVIC
+	uint32_t *pISER1 = (u_int32_t*)0xE000E104;
+	*pISER1 |= (1 << (USART3_IRQNO % 32));
+
 	for(;;);
+}
+
+// USART3 ISR
+void USART3_IRQHandler(void)
+{
+	printf("\t -> In USART3 ISR\n");
 }
